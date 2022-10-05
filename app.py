5f50feb3b3de97d1811bfe5eb2b73bd558742ab9
storm <storm@ROG.localdomain>
2022-10-05 21:02:30 +0530

    add template

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
+import gtts
 app = Flask(__name__)
 
+ 
+
 @app.route('/')
-def hello_world():
-    return 'Hello Sammy!'
+@app.route('/home')
+def home():
+    return render_template("index.html")
+
+
+
+@app.route('/result',methods=['POST', 'GET'])
+def result():
+    text = request.form['speech']
+    tts1 = gtts.gTTS(text, lang="en")
+    tts1.save("templates/gttsspeech.mp3")
+    return render_template('index.html', name = text)
+    
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
